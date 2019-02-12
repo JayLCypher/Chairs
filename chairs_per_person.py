b6ef0365aa3d59024a57239bee17cd46008ed92d
John L C <JayLCypher@users.noreply.github.com>
2019-02-12 15:18:28 +0100

    Fixed the exceptions from reservation.csv
    Two exceptions found, ValueError and ZeroDivisionError, both sorted by exception handling.
    Fixed file to be more clear.
    Fixed file not being closed (potential memory leak?)
    Syntaxing fixed

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,21 @@
-f = open("reservations.csv")
+import os, sys
+
+f = open(os.path.join(sys.path[0],'reservations.csv'), 'r')
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
-    print("{} will get {} chairs per person".format(name, chairs_per_person))
+
+    try:
+        chairs_per_person = 50 / int(number)
+
+    except ValueError:
+        print("{} won't get squat because {} is not an integer number".format(name, number.rstrip("\r,\n")),"\n")
+    
+    except ZeroDivisionError:
+        print("Don't divide by zero you dimwit","\n")
+
+    else:
+        print("{} will get {} chairs per person".format(name, chairs_per_person),"\n")
+
+print("End of file reached. Closing file...")
+f.close()
